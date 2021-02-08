7ff1ed796f5c01262ee96198a7b81498a391dbe5
chekelon <cheke_11@hotmail.com>
2021-02-07 18:00:31 -0600

    algunos cambios

--- main.py
+++ main.py
@@ -9,31 +9,20 @@ from saveJson import *
 def iniciar():
 	opcion=-1
 	fullListPerson()
-	#eleccion=True
 	
 	while opcion!=9:
-		# if eleccion:
-		# 	print("Con que BD desea trabajar..\n"
-		# 		"1) MYSQL \n"
-		# 		"2) MONGO\n\n")
-		# 	eleccion=False
-		# 	basededatos=int(input("Escribe el numero : "))
-		# 	if basededatos == 1 or basededatos == 2: 
-		# 		peticion.whatDB(basededatos)
-				
 
-
-		print(":::::PRESTAMO DE MATERIAL DEPORTIVO::::: \n\n"
-			"Elija una Opcion....\n\n"
-			"1) Listado de prestamos..\n"
-			"2) Prestar...\n"
-			"3) Eliminar prestamo\n"
-			"4) Modificar prestamo\n"
-			"5) Entrega...\n"
-			"6) Registrar nueva persona...\n"
-			"7) Eliminar persona\n"
-			"8) Modificar persona\n"
-			"9) Salir.....\n")
+		print(":::::PRESTAMO DE MATERIAL DEPORTIVO:::::\n\n"
+			  "Elija una Opcion....\n\n"
+			  "1) Listado de prestamos..\n"
+			  "2) Prestar...\n"
+			  "3) Eliminar prestamo\n"
+			  "4) Modificar prestamo\n"
+			  "5) Entrega...\n"
+			  "6) Registrar nueva persona...\n"
+			  "7) Eliminar persona\n"
+			  "8) Modificar persona\n"
+			  "9) Salir.....\n")
 
 		opcion=int(input("Escribir Opcion..."))
 
